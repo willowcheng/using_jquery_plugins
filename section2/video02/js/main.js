2af5697fce9f6f4c047eca4099c2c64d4ec7c574
willowcheng <e.wingus@Gmail.com>
2015-11-17 19:18:15 -0500

    Plugin events

--- section2/video02/js/main.js
+++ section2/video02/js/main.js
@@ -1,9 +1,17 @@
 $(".animsition").animsition({
-  inClass: 'fade-in-right-lg',
-  outClass: 'fade-out-right-lg',
-  linkElement: 'header a',
-  inDuration: 1000,
-  outDuration: 500
+    inClass: 'fade-in-right-lg',
+    outClass: 'fade-out-right-lg',
+    linkElement: 'header a',
+    inDuration: 1000,
+    outDuration: 500
 });
 
 $('.header').sticky();
+
+$('.header').on('sticky-start', function () {
+    $('.description').html('We build <strong>great</strong> apps');
+});
+
+$('.header').on('sticky-end', function () {
+    $('.description').html('We build apps');
+});
